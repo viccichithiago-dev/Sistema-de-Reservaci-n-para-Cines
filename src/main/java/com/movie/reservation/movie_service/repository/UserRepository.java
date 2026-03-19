709e58a9cf7b6a5841face23cb3ad404de937ac2
Thiago Viccichi <viccichithiago@gmail.com>
2026-03-18 22:01:44 -0300

    Modificaciones debido a errores en el repository

--- src/main/java/com/movie/reservation/movie_service/repository/UserRepository.java
+++ src/main/java/com/movie/reservation/movie_service/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.movie.reservation.movie_service.repository;
-import com.movie.reservation.movie_service.model.User;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
-import java.util.List;
+
+import com.movie.reservation.movie_service.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
         // Authenticacion y busquedas criticas
@@ -15,10 +17,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
         boolean existsByUsername(String username);
 
         // Soporte para poder paginar y ordenar
+        @Override
         Page<User> findAll(Pageable pageable);
         List<User> findAllByOrderByUsernameAsc();
-
-        // Utilidades
-        boolean existsById(Long id);
-        void deleteById(Long id);
 }
