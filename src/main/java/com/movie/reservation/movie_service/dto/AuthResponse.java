a1974d46fdb59c353e784e0515d872405cb28723
Thiago Viccichi <viccichithiago@gmail.com>
2026-04-04 19:26:59 -0300

    Agregacion de validaciones para MovieRequest y mejora en los atributos de los demas dtos

--- src/main/java/com/movie/reservation/movie_service/dto/AuthResponse.java
+++ src/main/java/com/movie/reservation/movie_service/dto/AuthResponse.java
@@ -1,9 +1,8 @@
 package com.movie.reservation.movie_service.dto;
 
-import java.time.LocalDateTime;
-
 public record AuthResponse(
     String token,
-    LocalDateTime expiresIn,
-    String userInfo
+    String email,
+    String role,
+    long expiresIn
 ) {}
--- src/main/java/com/movie/reservation/movie_service/dto/LoginRequest.java
+++ src/main/java/com/movie/reservation/movie_service/dto/LoginRequest.java
@@ -1,6 +1,11 @@
 package com.movie.reservation.movie_service.dto;
-
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 public record LoginRequest(
+
+    @Email
+    @NotBlank
     String email,
+    @NotBlank
     String password
 ) {}
