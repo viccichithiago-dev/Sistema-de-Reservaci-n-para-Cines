3d845e3def831186fb2e45d167ae1a5d993109bf
Thiago Viccichi <viccichithiago@gmail.com>
2026-04-03 21:53:25 -0300

    Agregacion y mejora de los modelos para poder realizar operaciones

--- src/main/java/com/movie/reservation/movie_service/model/Reservation.java
+++ src/main/java/com/movie/reservation/movie_service/model/Reservation.java
@@ -1,10 +1,15 @@
 package com.movie.reservation.movie_service.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,10 +19,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
