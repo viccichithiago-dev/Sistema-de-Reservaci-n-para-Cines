1915e7bae9b841fbeb088036f6f0e800ad7c1119
Thiago Viccichi <viccichithiago@gmail.com>
2026-04-07 23:10:57 -0300

    Mejoras en el codigo y implementacion del repositorio de las peliculas

--- src/main/java/com/movie/reservation/movie_service/repository/MovieRepository.java
+++ src/main/java/com/movie/reservation/movie_service/repository/MovieRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,7 +16,7 @@ import com.movie.reservation.movie_service.model.Genre;
 import com.movie.reservation.movie_service.model.Movie;
 
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Long>{
+public interface MovieRepository extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {
     // Essential search methods for movie catalog
     // Text searches
     List<Movie> findByTitleContainingIgnoreCase(String title);
