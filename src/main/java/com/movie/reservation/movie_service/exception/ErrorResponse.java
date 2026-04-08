ac76531ba4b38a5e86951e1a960d604de29fb115
Thiago Viccichi <viccichithiago@gmail.com>
2026-04-07 23:06:52 -0300

    DTO para devolver los Errores en formate JSON

--- src/main/java/com/movie/reservation/movie_service/exception/ErrorResponse.java
+++ src/main/java/com/movie/reservation/movie_service/exception/ErrorResponse.java
@@ -1,6 +1,6 @@
 package com.movie.reservation.movie_service.exception;
 import java.time.LocalDateTime;
-
+// DTO para representar la respuesta de error en formato JSON
 public record ErrorResponse(
     int status,
     String message,
