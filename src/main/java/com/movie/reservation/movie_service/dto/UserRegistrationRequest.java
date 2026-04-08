4b12cb5763f88d70039824dd8024f982a9ccf787
Thiago Viccichi <viccichithiago@gmail.com>
2026-04-07 23:10:23 -0300

    Mejoras en el DTO, cambios validos

--- src/main/java/com/movie/reservation/movie_service/dto/UserRegistrationRequest.java
+++ src/main/java/com/movie/reservation/movie_service/dto/UserRegistrationRequest.java
@@ -1,7 +1,15 @@
 package com.movie.reservation.movie_service.dto;
-
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 public record UserRegistrationRequest(
+    @Email
+    @NotBlank
     String email,
+    @NotBlank
+    @Size(min = 6)
     String password,
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "El nombre de usuario solo puede contener letras, números y guiones bajos")
     String name
 ) {}
