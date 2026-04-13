63fdfeee95a557778925f80446ae5d6369c99f39
Thiago Viccichi <viccichithiago@gmail.com>
2026-04-13 15:15:44 -0300

    Actualizar método getAvailableShowtimes en ShowtimeService para usar paginación y ajustar parámetros de entrada

--- src/main/java/com/movie/reservation/movie_service/service/ShowtimeService.java
+++ src/main/java/com/movie/reservation/movie_service/service/ShowtimeService.java
@@ -3,14 +3,19 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.movie.reservation.movie_service.dto.ShowtimeRequest;
 import com.movie.reservation.movie_service.dto.ShowtimeResponse;
+import com.movie.reservation.movie_service.dto.AvailableSeatResponse;
 import com.movie.reservation.movie_service.model.Genre;
 
 public interface ShowtimeService {
     ShowtimeResponse createShowtime(ShowtimeRequest request);
     ShowtimeResponse getShowtimeById(Long id);
-    List<ShowtimeResponse> getAvailableShowtimes(LocalDate date, Optional<Genre> genreId);
+    Page<ShowtimeResponse> getAvailableShowtimes(Optional<LocalDate> date, Optional<Genre> genreId,Pageable pageable);
     ShowtimeResponse updateShowtime(Long id, ShowtimeRequest request);
     void deleteShowtime(Long id);
+    List<AvailableSeatResponse> getAvailableSeats(Long showtimeId);
 }
